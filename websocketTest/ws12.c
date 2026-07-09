fe7c2e55aadc1df24a0bf310b13be86e4e1ee8ce
IanWorldHi <ianchensmiley@gmail.com>
2026-07-09 19:39:38 -0400

    hi

--- websocketTest/ws12.c
+++ websocketTest/ws12.c
@@ -11,9 +11,21 @@ struct per_session_data_prot1{
     struct per_session_data_prot1 *pss_list; //linked list of all sessions
     struct lws *wsi; //websocket instance
     int last; //last msg sent num
+};
+
+//One for vhost
+struct per_vhost_data_prot1{
+    struct lws_context *context;
+    struct lws_vhost *vhost;
+    const struct lws_protocols *protocol;
+    struct per_session_data_prot1 *pss_list; 
+    struct msg *a_msg; //one pending msg
+    int current; //cuurent msg we are caching
+};
+
+static void __prot1_destroy_msg(void *_msg){ //why the underscore?
+
 }
-
-
 
 callbackFunc(){
 

8000fd6ac627fe0094f8ed4ad8c9c9ff78299464
IanWorldHi <ianchensmiley@gmail.com>
2026-07-10 14:19:42 -0400

    coffee tastes good

--- websocketTest/ws12.c
+++ websocketTest/ws12.c
@@ -30,15 +30,36 @@ static void __prot1_destroy_msg(void *_msg){ //why the underscore?
     msg->len = 0;
 }
 
-callbackFunc(){
+static int callbackFunc(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len){
+    struct per_session_data_prot1 *pss = (struct per_session_data_prot1 *)user;
+    struct per_vhost_data_prot1 *vhd = (struct per_vhost_data_prot1 *)lws_protocol_vh_priv_get(lws_get_vhost(wsi), lws_get_protocol(wsi));
+    int m;
 
+    switch(reason){
+        case LWS_CALLBACK_PROTOCOL_INIT:
+            
+            break;
+        case LWS_CALLBACK_ESTABLISHED:
+            break;
+        case LWS_CALLBACK_CLOSED:
+            break;
+        case LWS_CALLBACK_SERVER_WRITEABLE:
+            break;
+        case LWS_CALLBACK_RECEIVE:
+            break;
+        default:
+            break;
+    }
+    return 0;
 }
 
 #define CALLBACKPROTOCOL1 \ 
 { \
     "name?" \
     callbackFunc, \
-    sizeof(), \ 
+    sizeof(struct per_session_data_prot1), \ 
+    128, \
+    0, NULL, 0 \
 }
 // max size? other stuff in here gotta add
 
